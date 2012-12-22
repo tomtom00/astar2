254b94940f66412cf1874b4e1dc53366cc15e6c6
Thomas Breitbart <thomas.breitbart@gmx.net>
2012-12-22 23:36:49 +0100

    updated

--- AStarShortestPath2.java
+++ AStarShortestPath2.java
@@ -1,159 +1,112 @@
-/* ==========================================
- * JGraphT : a free Java graph-theory library
- * ==========================================
- *
- * Project Info:  http://jgrapht.sourceforge.net/
- * Project Creator:  Barak Naveh (http://sourceforge.net/users/barak_naveh)
- *
- * (C) Copyright 2003-2008, by Barak Naveh and Contributors.
- *
- * This library is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation; either version 2.1 of the License, or
- * (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
- * License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this library; if not, write to the Free Software Foundation,
- * Inc.,
- * 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
- */
-/* -------------------------
- * AStarShortestPath.java
- * -------------------------
- * (C) Copyright 2003-2008, by John V. Sichi and Contributors.
- *
- * Original Author:  Jon Robison
- *
- * $Id$
- *
- * Changes
- * -------
- * 10-Sep-2012 : Initial revision (JR);
- *
- */
 package org.jgrapht.alg;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-import org.jgrapht.*;
 import org.jgrapht.graph.*;
-
-/*
+import org.jgrapht.util.FibonacciHeap;
+import org.jgrapht.util.FibonacciHeapNode;
+import org.jgrapht.GraphPath;
+/**
  * An implementation of <a
  * href="http://en.wikipedia.org/wiki/A*_search_algorithm">A* shortest 
  * path algorithm</a>.
+ * <a href="http://de.wikipedia.org/wiki/A*-Algorithmus"> A* shortest path algorithm german Wiki </a>.
+ * 
+ * Inspired by Jon Robison
  *
- * @author Jon Robison
- * @since Sep 10, 2012
+ * @author Thomas Breitbart
+ * @since Sep, 2012
  */
-public final class AStarShortestPath2<V, E>
-{
-    //~ Instance fields --------------------------------------------------------
+
+public final class AStarShortestPath2<V, E> {
+	
+	//~ Instance fields --------------------------------------------------------
 
     private GraphPath<V, E> path;
 
     //~ Constructors -----------------------------------------------------------
 
-    /**
-     * Creates and executes a new AStarShortestPath algorithm instance. An
-     * instance is only good for a single search; after construction, it can be
-     * accessed to retrieve information about the path found.
-     * 
-     * This is mostly a copy of the wikipedia pseudocode entry, with I suppose
-     * the only notable exception being the use of treemap and its 
-     * corresponding comparator. That was used to more closely parallel the 
-     * wikis code.
-     * 
-     * @param graph the graph to be searched
-     * @param startVertex the vertex at which the path should start
-     * @param endVertex the vertex at which the path should end
-     * @param functionProvider of f(x) and g(x), estimating costs
-     */
-    public AStarShortestPath2(WeightedGraph<V, E> graph,
-        V startVertex,
-        V endVertex,
-        AStarFunctionProvider<V> functionProvider) {
-        if (!graph.containsVertex(endVertex))
+    public AStarShortestPath2(SimpleDirectedWeightedGraph<V, E> graph,
+            V startVertex,
+            V endVertex,
+            AStarFunctionProvider2<V> functionProvider) {
+    	
+    	if (!graph.containsVertex(endVertex))
             throw new IllegalArgumentException("graph must contain the end vertex");
         
-        List<V> closedSet = new ArrayList<V>(),
-        		openSet = new ArrayList<V>();   
-        Map<V,V> cameFrom = new HashMap<V,V>();
-        Map<V, Double> gScoreMap = new HashMap<V, Double>();
-        TreeMap<V, Double> fScoreMap = new TreeMap<V, Double>(new VertexComparator(functionProvider, endVertex));
-        gScoreMap.put(startVertex, 0.0);
-        fScoreMap.put(startVertex, functionProvider.getHeuristicCost(startVertex, endVertex));
-        openSet.add(startVertex);    
-        
-        while(!openSet.isEmpty()){
-        	V current = fScoreMap.firstKey();
-        	fScoreMap.remove(current);
-        	if(current == endVertex){
-        		path = buildGraphPath(cameFrom, current, graph, startVertex, endVertex);
+    	//Die benötigte OpenList
+    	FibonacciHeap<V> openList = new FibonacciHeap<V>();
+    	List<V> openListcheck = new ArrayList<V>();
+    	//Die benötigte ClosedList
+    	List<V> closedList = new ArrayList<V>();
+    	//Merken der g-Werte
+    	Map<V, Double> gScoreMap = new HashMap<V, Double>();
+    	//Karte der Vorgänger/Nachfolger
+    	 Map<V,V> cameFrom = new HashMap<V,V>();
+    	
+    	gScoreMap.put(startVertex, 0.0);
+    	openList.insert(new FibonacciHeapNode<V>(startVertex), 0.0);
+    	openListcheck.add(startVertex);
+    	
+    	do {
+    		FibonacciHeapNode<V> currentNode = openList.removeMin();
+    		//sind wir schon am Ziel?
+    		if (currentNode.getData() == endVertex){
+    			//baue hier den Pfad
+    			path = buildGraphPath(cameFrom, currentNode.getData(), graph, startVertex, endVertex);
         		return;
-        	}
-        	expandNode(current);
-        	closedSet.add(current);
-        }
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-
-
-        
-
-
-        while(!openSet.isEmpty()){
-        	
-        	openSet.remove(current);
-        	closedSet.add(current);
-        	for(E edge : graph.edgesOf(current)){
-        		V neighbor = graph.getEdgeTarget(edge);
-        		if(closedSet.contains(neighbor))
-        			continue;
-        		double tentativeGScore = gScoreMap.get(current) + functionProvider.getPathCost(current, neighbor);
-        		
-        		if(!openSet.contains(neighbor) || tentativeGScore < gScoreMap.get(neighbor)){
-        			if(!openSet.contains(neighbor))
-        				openSet.add(neighbor);
-        			cameFrom.put(neighbor, current);
-        			gScoreMap.put(neighbor, tentativeGScore);
-        			fScoreMap.put(neighbor, gScoreMap.get(neighbor) + functionProvider.getHeuristicCost(neighbor, endVertex));
-        		}
-        	}
-        }
+    		}
+    		//Noch nicht am Ziel, also expandiere den Knoten
+    		expandNode(currentNode, graph, closedList, openList, gScoreMap, functionProvider, closedList, cameFrom, endVertex);
+    		closedList.add(currentNode.getData());
+    	} while(!openList.isEmpty());
+    	//hier wurde kein Pfad gefunden
+    	path=null;
     }
     
-    private void expandNode(V current, WeightedGraph<V, E> graph){
-    	for(E edge : graph.edgesOf(current)){
-    		V neighbor = graph.getEdgeTarget(edge);
-    		if(closedSet.contains(neighbor))
+    private void expandNode(FibonacciHeapNode<V> currentNode, 
+    		SimpleDirectedWeightedGraph<V, E> graph, 
+    		List<V> closedList, 
+    		FibonacciHeap<V> openList, 
+    		Map<V, Double> gScoreMap, 
+    		AStarFunctionProvider2<V> functionProvider,
+    		List<V> openListcheck, 
+    		Map<V,V> cameFrom,
+    		V endVertex){
+    	
+    	for(E edge : graph.edgesOf(currentNode.getData())){
+    		//keine Knoten die auf sich selbst verweisen! Erzeugt sonst unter Umständen Loops
+    		V target = graph.getEdgeTarget(edge);
+    		if (target == currentNode.getData()){
     			continue;
-    		double tentativeGScore = gScoreMap.get(current) + functionProvider.getPathCost(current, neighbor);
+    		}
+    		V successor = graph.getEdgeTarget(edge);
+    		if (closedList.contains(successor)){
+    			continue;
+    		}
+    		double gScore_current = gScoreMap.get(currentNode.getData());
+    		double tentativeGScore = gScore_current + functionProvider.getPathCost(currentNode.getData(), successor);
     		
-    		if(!openSet.contains(neighbor) || tentativeGScore < gScoreMap.get(neighbor)){
-    			if(!openSet.contains(neighbor))
-    				openSet.add(neighbor);
-    			cameFrom.put(neighbor, current);
-    			gScoreMap.put(neighbor, tentativeGScore);
-    			fScoreMap.put(neighbor, gScoreMap.get(neighbor) + functionProvider.getHeuristicCost(neighbor, endVertex));
+    		if (openListcheck.contains(successor) && tentativeGScore >= gScoreMap.get(successor)){
+    			continue;
+    		}
+    		cameFrom.put(successor, currentNode.getData());
+    		gScoreMap.put(successor, tentativeGScore);
+    		
+    		double f= tentativeGScore + functionProvider.getHeuristicCost(successor, endVertex);
+    		if (openListcheck.contains(successor)) {
+    			openList.decreaseKey(new FibonacciHeapNode<V>(successor), f);
+    		}
+    		else {
+    			openList.insert(new FibonacciHeapNode<V>(successor), f);
+    			openListcheck.add(successor);
     		}
     	}
     }
-
+    
     private List<V> buildPath(Map<V,V> cameFrom, V currentNode){
     	List<V> path = cameFrom.containsKey(currentNode) ? 
     			buildPath(cameFrom, cameFrom.get(currentNode)) :
@@ -163,7 +116,7 @@ public final class AStarShortestPath2<V, E>
     }
 
     private GraphPath<V, E> buildGraphPath(Map<V,V> cameFrom, V currentNode,
-    		WeightedGraph<V, E> graph, V startVertex, V endVertex){
+    		SimpleDirectedWeightedGraph<V, E> graph, V startVertex, V endVertex){
     	List<V> reconstructed = buildPath(cameFrom, currentNode);
 		List<E> edgeList = new ArrayList<E>();
 		double weight = 0.0;
@@ -174,12 +127,7 @@ public final class AStarShortestPath2<V, E>
 		}
 		return new GraphPathImpl<V, E>(graph, startVertex, endVertex, edgeList, weight);
     }
-
-    /**
-     * Return the edges making up the path found.
-     *
-     * @return List of Edges, or null if no path exists
-     */
+    
     public List<E> getPathEdgeList()
     {
         if (path == null) {
@@ -212,36 +160,8 @@ public final class AStarShortestPath2<V, E>
             return path.getWeight();
         }
     }
-
-    /**
-     * Convenience method to find the shortest path via a single static method
-     * call. If you need a more advanced search (e.g. limited by radius, or
-     * computation of the path length), use the constructor instead.
-     *
-     * @param graph the graph to be searched
-     * @param startVertex the vertex at which the path should start
-     * @param endVertex the vertex at which the path should end
-     * @param functionProvider of f(x) and g(x), estimating costs
-     *
-     * @return List of Edges, or null if no path exists
-     */
-    public static <V, E> List<E> findPathBetween(
-        WeightedGraph<V, E> graph,
-        V startVertex,
-        V endVertex,
-        AStarFunctionProvider<V> functionProvider)
-    {
-        AStarShortestPath2<V, E> alg =
-            new AStarShortestPath2<V, E>(
-                graph,
-                startVertex,
-                endVertex,
-                functionProvider);
-
-        return alg.getPathEdgeList();
-    }
     
-    public interface AStarFunctionProvider<V>{
+    public interface AStarFunctionProvider2<V>{
     	/**
          * An admissible "heuristic estimate" of the distance from x to the goal 
          * (usually denoted h(x)). This is the good guess function.
@@ -253,22 +173,4 @@ public final class AStarShortestPath2<V, E>
     	 */
     	public double getPathCost(V neighbor, V goal);
     }
-    
-    private class VertexComparator implements Comparator<V>{
-    	private final AStarFunctionProvider<V> provider;
-    	private final V goal;
-    	
-    	private VertexComparator(AStarFunctionProvider<V> provider, V goal){
-    		this.provider = provider;
-    		this.goal = goal;
-    	}
-    	
-		@Override public int compare(V o1, V o2) {
-			Double o1Distance = provider.getPathCost(o1, goal),
-				o2Distance = provider.getPathCost(o2, goal);
-			return o1Distance.compareTo(o2Distance);
-		}
-    }
 }
-
-// End AStarShortestPath.java
